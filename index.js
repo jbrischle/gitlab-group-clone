eedb5bf4d7206b0bae129bb52f65f7f602d770fb
Janick Brischle <janick.brischle@de.markant.com>
2021-10-14 00:35:00 +0200

    add files

---
+++ index.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const clone = require('git-clone');
+
+const gitlabApiUrl = 'https://gitlab.markant.com/api/v4/groups/';
+const groupId = 557;
+const privateToken = 'XXX';
+const baseDir = 'C:\\GIT';
+
+const options = {
+    'method': 'GET',
+    'url': gitlabApiUrl + groupId + '/projects?archived=false&simple=true&per_page=100',
+    'headers': {
+        'PRIVATE-TOKEN': privateToken,
+        'Content-Type': 'application/json',
+    }
+};
+request(options, function (error, response) {
+    if (error) throw new Error(error);
+    process.chdir(baseDir);
+    Array.from(JSON.parse(response.body)).forEach(project => {
+        const targetPath = project.namespace.full_path + ' / ' + project.path;
+        clone(project.ssh_url_to_repo, targetPath);
+        console.log('cloned: ' + project.web_url + ' to ' + targetPath)
+    });
+});
